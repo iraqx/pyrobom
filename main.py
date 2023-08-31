3eeed72ac74eb38873461035b59f5c88bd53961e
iraqx <82683837+iraqx@users.noreply.github.com>
2023-08-31 04:47:25 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,63 @@
-# bot.py
+from telethon.sync import TelegramClient, events
+import yt_dlp
 
-import os
-from pyrogram import Client, filters
-from yt_dlp import YoutubeDL
+# Telegram API credentials
+api_id = 11319462
+api_hash = '155d33dec6ee17ca6135c0a6e01c1129'
+bot_token = 'YOUR_BOT_TOKEN'  # Replace with your bot token
 
-api_id = 27495900
-api_hash = 'f5e05aadcfca1c374b55fdad2fe2029e'
-bot_token = '6460565675:AAHSlI5R6SLwy9qjjxNgfNLTaK1opaY--1I'
+# Initialize the Telethon client
+client = TelegramClient('bot_session', api_id, api_hash)
 
-app = Client("video_bot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
+# Event handler for /start command
+@client.on(events.NewMessage(pattern='/start'))
+async def start_command(event):
+    await event.reply("`Hi... `")
 
-@app.on_message(filters.command("video"))
-async def video_handler(_, message):
+# Convert duration to readable format
+def format_duration(duration):
+    if duration < 60:
+        return f"{duration} sec"
+    elif duration < 3600:
+        return f"{duration // 60} min"
+    else:
+        return f"{duration // 3600} hour"
 
-  try:
-    url = message.text.split(None, 1)[1]
-  except IndexError:
-    await message.reply("Please provide a URL to download.")
-    return
+# Event handler for incoming messages
+@client.on(events.NewMessage)
+async def process_video_link(event):
+    message = event.message
+    chat_id = message.chat_id
+    text = message.text
+    
+    if text.startswith('https://www.youtube.com/') or text.startswith('https://youtu.be/'):
+        ydl_opts = {
+            'format': 'best',
+            'outtmpl': 'downloads/%(title)s.%(ext)s',
+        }
+        
+        # Send a "Downloading..." message to the chat
+        downloading_message = await client.send_message(chat_id, f"`Downloading...`")
+        
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(text, download=True)
+            video_path = os.path.join('downloads', info_dict['title'] + '.' + info_dict['ext'])
+            
+            # Upload the video to the chat
+            uploading_message = await client.send_file(chat_id, video_path, reply_to=downloading_message)
+            await client.delete_messages(chat_id, [downloading_message.id])  # Delete the "Downloading..." message
+            
+            # Convert duration to readable format
+            duration_formatted = format_duration(int(info_dict['duration']))
+            
+            # Update the message to display video information
+            video_info = f"Video Downloaded:\nTitle: {info_dict['title']}\nDuration: {duration_formatted}"
+            await client.edit_message(chat_id, uploading_message.id, video_info)
+            
+            # Remove the downloaded video from storage
+            os.remove(video_path)
+            
 
-  await message.reply("`Downloading...`")
-
-  ydl_opts = {"outtmpl": "%(id)s.mp4"}
-  with YoutubeDL() as ydl:
-    ydl.download([url])
-
-  video = open(f"{message.message_id}.mp4", "rb")
-
-  try:
-    await message.reply_video(video) 
-  finally:
-    video.close()
-    os.remove(f"{message.message_id}.mp4")
-
-  await message.delete()
-
-app.run()
+# Start the bot
+client.start(bot_token=bot_token)
+client.run_until_disconnected()

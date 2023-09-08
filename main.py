7a0089e167924e70d2384a81e2e0cb0e921e897f
iraqx <82683837+iraqx@users.noreply.github.com>
2023-09-09 02:28:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
 from telethon.sync import TelegramClient, events
 import yt_dlp
+import os
 
 api_id = 11319462
 api_hash = '155d33dec6ee17ca6135c0a6e01c1129'
@@ -10,17 +10,7 @@ client = TelegramClient('bot_session', api_id, api_hash)
 
 @client.on(events.NewMessage(pattern='/start'))
 async def start_command(event):
-    await event.reply("`Hi... `")
-
-def format_duration(duration):
-    if duration < 60:
-        return f"{duration} sec"
-    elif duration < 3600:
-        return f"{duration // 60} min"
-    else:
-        return f"{duration // 3600} hour"
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    await event.reply("`🗿 `")
 
 # Event handler for incoming messages
 @client.on(events.NewMessage)
@@ -31,8 +21,8 @@ async def process_video_link(event):
 
     if text.startswith('https://'):
         ydl_opts = {
-            'format': 'best',
-            'outtmpl': 'downloads/%(title)s.%(ext)s',
+            'format': 'bestvideo+bestaudio/best',  # Get the best quality video
+            'outtmpl': 'downloads/%(id)s.%(ext)s',  # Save with ID as filename
         }
 
         # Send an initial "Downloading..." message to the chat
@@ -40,22 +30,22 @@ async def process_video_link(event):
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(text, download=True)
-            video_path = os.path.join('downloads', info_dict['title'] + '.' + info_dict['ext'])
+            video_path = os.path.join('downloads', info_dict['id'] + '.' + info_dict['ext'])
 
             # Replace the "Downloading..." message with "Uploading..."
             uploading_message = await client.edit_message(chat_id, downloading_message.id, f"`Uploading...`")
 
             # Upload the video to the chat
             video = await client.upload_file(video_path, file_name=video_path)
-            video_info = f"`Title: {info_dict['title']}\nDuration: {format_duration(int(info_dict['duration']))}`"
-            await client.send_file(chat_id, file=video, reply_to=downloading_message, caption=video_info)
+            await client.send_file(chat_id, file=video, reply_to=downloading_message)
 
             # Remove the uploaded video from storage
             os.remove(video_path)
 
             # Delete the "Uploading..." message
             await client.delete_messages(chat_id, [uploading_message.id])
-
+    else:
+        await client.send_message(chat_id, f"`Please send a valid link`")
 
 client.start(bot_token=bot_token)
 client.run_until_disconnected()

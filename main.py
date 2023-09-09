ca1b0fcb94dc9be004c319943b3e196931d355be
iraqx <82683837+iraqx@users.noreply.github.com>
2023-09-09 05:35:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 from telethon.sync import TelegramClient, events
 import yt_dlp
 import os
+import asyncio
 
 api_id = 11319462
 api_hash = '155d33dec6ee17ca6135c0a6e01c1129'
-bot_token = '5718397874:AAGwjNGnv95LuBANzYOuGi4tu1CGe1e9r-c'
+bot_token = '5718397874:AAGwjNGnv95LuBANzYOuGi4tu1CGe1e9r-c'  # يمكنك تغييره إلى التوكن الذي تريده
 
 client = TelegramClient('bot_session', api_id, api_hash)
 
-@client.on(events.NewMessage(pattern='/start'))
-async def start_command(event):
-    await event.reply("`🗿 `")
-
-# Event handler for incoming messages
 @client.on(events.NewMessage)
-async def process_video_link(event):
+async def process_messages(event):
     message = event.message
     chat_id = message.chat_id
     text = message.text
 
-    if text.startswith('https://'):
+    if text == '/start':
+        await event.reply("`send link `")
+    elif text.startswith('https://'):
         ydl_opts = {
             'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',  # Get the best quality MP4 video
             'outtmpl': 'downloads/%(id)s.%(ext)s',  # Save with ID as filename
@@ -29,20 +27,29 @@ async def process_video_link(event):
         downloading_message = await client.send_message(chat_id, f"`Downloading...`")
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(text, download=True)
-            video_path = os.path.join('downloads', info_dict['id'] + '.' + 'mp4')
+            try:
+                info_dict = ydl.extract_info(text, download=True)
+                video_path = os.path.join('downloads', info_dict['id'] + '.' + 'mp4')
+            except Exception as e:
+                # If there's an error in downloading, try with the best quality
+                ydl_opts['format'] = 'best'
+                info_dict = ydl.extract_info(text, download=True)
+                video_path = os.path.join('downloads', info_dict['id'] + '_best' + '.' + 'mp4')
 
-            # Replace the "Downloading..." message with "Uploading..."
-            uploading_message = await client.edit_message(chat_id, downloading_message.id, f"`Uploading...`")
-
-            # Upload the video to the chat
-            video = await client.upload_file(video_path, file_name=video_path)
-            await client.send_file(chat_id, file=video, reply_to=downloading_message)
+            # Send the video to the chat
+            await client.send_file(
+                chat_id,
+                video_path,
+                reply_to=downloading_message
+            )
 
             # Remove the uploaded video from storage
             os.remove(video_path)
 
-            # Delete the "Uploading..." message
-            await client.delete_messages(chat_id, [uploading_message.id])
+            # Delete the "Downloading..." message
+            await client.delete_messages(chat_id, [downloading_message.id])
+    elif not text.startswith('https://'):
+        await client.send_message(chat_id, f"`invalid link`")
+
 client.start(bot_token=bot_token)
 client.run_until_disconnected()

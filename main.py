8e125c52147ce6ef09028483cb01c1fbef2358b7
iraqx <82683837+iraqx@users.noreply.github.com>
2023-09-09 13:52:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,46 @@
-from telethon.sync import TelegramClient, events
-import yt_dlp
 import os
-import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from yt_dlp import YoutubeDL
 
 api_id = 11319462
 api_hash = '155d33dec6ee17ca6135c0a6e01c1129'
-bot_token = '5718397874:AAGwjNGnv95LuBANzYOuGi4tu1CGe1e9r-c'  # يمكنك تغييره إلى التوكن الذي تريده
+bot_token = '5718397874:AAGwjNGnv95LuBANzYOuGi4tu1CGe1e9r-c'
+app = Client("uuhhbot", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-client = TelegramClient('bot_session', api_id, api_hash)
+@app.on_message(filters.command("start"))
+def start_command_handler(client: Client, message: Message):
+    message.reply_text("`send link`")
 
-@client.on(events.NewMessage)
-async def process_messages(event):
-    message = event.message
-    chat_id = message.chat_id
-    text = message.text
+def download_video(url: str, message: Message):
+    ydl_opts = {
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best',
+        'outtmpl': '%(id)s.%(ext)s',
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        try:
+            downloading_message = message.reply_text("`Downloading...`")
+            info = ydl.extract_info(url, download=True)
+            video_id = info.get('id', None)
+            if video_id:
+                video_file = f"{video_id}.mp4"
+                
+                message.reply_video(video_file)
+                downloading_message.delete()
+                os.remove(video_file)
+            else:
+                downloading_message.edit_text("`An error occurred while downloading the video.`")
+        except Exception as e:
+            # تجاهل الاستثناء واستمر في التنفيذ
+            pass
 
-    if text == '/start':
-        await event.reply("`send link `")
-    elif text.startswith('https://'):
-        ydl_opts = {
-            'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',  # Get the best quality MP4 video
-            'outtmpl': 'downloads/%(id)s.%(ext)s',  # Save with ID as filename
-        }
+@app.on_message(filters.regex(r"(?i)^(https?://).+$"))
+def http_url_handler(client: Client, message: Message):
+    url = message.text.strip()
+    download_video(url, message)
 
-        # Send an initial "Downloading..." message to the chat
-        downloading_message = await client.send_message(chat_id, f"`Downloading...`")
+@app.on_message(filters.regex(r".+"))
+def unsupported_url_handler(client: Client, message: Message):
+    message.reply_text("`link not support`")
 
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            try:
-                info_dict = ydl.extract_info(text, download=True)
-                video_path = os.path.join('downloads', info_dict['id'] + '.' + 'mp4')
-            except Exception as e:
-                # If there's an error in downloading, try with the best quality
-                ydl_opts['format'] = 'best'
-                info_dict = ydl.extract_info(text, download=True)
-                video_path = os.path.join('downloads', info_dict['id'] + '_best' + '.' + 'mp4')
-
-            # Send the video to the chat
-            await client.send_file(
-                chat_id,
-                video_path,
-                reply_to=downloading_message
-            )
-
-            # Remove the uploaded video from storage
-            os.remove(video_path)
-
-            # Delete the "Downloading..." message
-            await client.delete_messages(chat_id, [downloading_message.id])
-    elif not text.startswith('https://'):
-        await client.send_message(chat_id, f"`invalid link`")
-
-client.start(bot_token=bot_token)
-client.run_until_disconnected()
+app.run()

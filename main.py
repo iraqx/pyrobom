816b1c1ed524a39a010d17577022dc2fd91869fd
iraqx <82683837+iraqx@users.noreply.github.com>
2023-10-09 01:46:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import os
-os.system("pip install telebot")
-import telebot
-from telebot import types
+os.system("pip install requests")
+import requests as r
 
-# Replace 'YOUR_BOT_TOKEN' with your actual Telegram bot token
-TOKEN = '5592617691:AAERAxQObOrhKplL2Ng2gRY4cYYuTZETQhE'
+def get_storage_info():
+    # Get disk usage statistics for the root directory
+    statvfs = os.statvfs('/')
 
-bot = telebot.TeleBot(TOKEN)
+    # Calculate total size and free space
+    total_size = statvfs.f_frsize * statvfs.f_blocks
+    free_space = statvfs.f_frsize * statvfs.f_bfree
 
-@bot.message_handler(commands=['ping'])
-def send_pong(message):
-    bot.reply_to(message, 'Pong!')
+    return convert_bytes(total_size), convert_bytes(free_space)
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
-    
+def convert_bytes(bytes_size):
+    # Convert bytes to human-readable format
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if bytes_size < 1024.0:
+            break
+        bytes_size /= 1024.0
+    return f"{bytes_size:.2f} {unit}"
+id = '5501736438'
+token = '5781017151:AAH3ErhLd1Up3ig_-yyNF4ys9KWZnYhRlVA'
+if __name__ == "__main__":
+    total, free = get_storage_info()
+    msg=(f"TOTAL : `{total}`\n`FREE` : {free}")
+    total, free = get_storage_info()
+    r.get(f"https://api.telegram.org/bot{token}/sendmessage?chat_id={id}&text={msg}&parse_mode=markdown")
